884945d9e09e8e02d14cc13f1ca5ed2e734c4593
Lucas Guirardel <l.guirardel@sms.ed.ac.uk>
2023-10-24 10:42:12 +0100

    Arbitrary metrics

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data_utils import *
 from models import *
 from contrastive_data import make_loaders
 
-from argparse import ArgumentParser
+from configargparse import ArgumentParser
 import torch 
 from torch import Tensor
 import torch.nn as nn
@@ -13,6 +13,7 @@ from glob import glob
 import pandas as pd
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
+import json
 
 
 
@@ -33,22 +34,34 @@ def get_paths(data_dir:str):
         paths.extend(l)
     return paths
 
+def write_metrics(metrics:  Dict[str, float|Dict], writer:SummaryWriter, main_tag:str, i):
+    for tag, metric_or_dict in metrics.items():
+        if isinstance(metric_or_dict, dict):
+            writer.add_scalars(f'{main_tag}_{tag}', metric_or_dict, i)
+        else:
+            writer.add_scalar(f'{main_tag}_{tag}', metric_or_dict, i)
+
+
         
-def train_model(train, test_seen, test_unseen, model,
-                 loss_fn, device, margin, n_epoch=100, run_name='run'):
+def train_model(train, test_seen, test_unseen, model, model_args:Dict,
+                 loss_fn, device, margin, n_epoch=100, run_name='run',
+                 ):
+    i_0 = model_args['restart']
     optimizer = torch.optim.AdamW(model.parameters())
-    bar = tqdm(range(n_epoch), position=0)
+    bar = tqdm(range(i_0, i_0+n_epoch), position=0)
     writer = SummaryWriter(join('runs',run_name))
     for i in bar:
+        bar.set_postfix({'i':i})
         loss_train = train_loop(train, model, loss_fn, optimizer, device)
         writer.add_scalar('train/loss',np.mean(loss_train), i)
-        loss_seen, acc_seen = test_loop(test_seen, model, loss_fn, device, margin=margin)
-        loss_unseen, acc_unseen = test_loop(test_unseen, model, loss_fn, device, margin=margin)
-        writer.add_scalar('test/loss_unseen',np.mean(loss_unseen), i)
-        writer.add_scalar('test/loss_seen',np.mean(loss_seen), i)
-        writer.add_scalar('test/acc_unseen',np.mean(acc_unseen), i)
-        writer.add_scalar('test/acc_seen',np.mean(acc_seen), i)
+        metrics_seen =  test_loop(test_seen, model, loss_fn, device, margin=margin)
+        metrics_unseen = test_loop(test_unseen, model, loss_fn, device, margin=margin)
+        write_metrics(metrics_seen, writer, 'test/seen',i)
+        write_metrics(metrics_unseen, writer, 'test/unseen',i)
         torch.save(model, join('models', run_name +  '.model.pkl'))
+        model_args['restart'] = i
+        with open(join('models',run_name+'.meta.json'), 'w') as file:
+            json.dump(model_args, file)
     writer.flush()
 
 
@@ -67,10 +80,13 @@ def train_loop(train:DataLoader, model:nn.Module, loss_fn:ContrastiveLoss, optim
         loss_l.append(loss)
     return [t.detach().cpu().item() for t in loss_l]
 
-def test_loop(test:DataLoader, model:nn.Module, loss_fn, device, margin) -> Tuple[Tensor]:
+def test_loop(test:DataLoader, model:nn.Module, loss_fn:ContrastiveLoss, device, margin, ) -> Dict[str, float|Dict]:
     model.eval()
     loss_l = []
     acc_l = []
+    y_l = []
+    d_l = []
+    norm_l = []
     with torch.no_grad():
         for x,y in tqdm(test, position=1, desc='Testing loop', leave=False):
             x = (x_i.to(device) for x_i in x)
@@ -79,9 +95,23 @@ def test_loop(test:DataLoader, model:nn.Module, loss_fn, device, margin) -> Tupl
             loss:Tensor = loss_fn(*embeddings, y)
             loss_l.append(loss)
             acc_l.append(accuracy(*embeddings, y, margin))
-    loss_l = [t.detach().cpu().item() for t in loss_l]
-    acc_l = [t.detach().cpu().item() for t in acc_l]
-    return loss_l, acc_l
+            y_l.append(y)
+            e1, e2 = embeddings
+            d_l.append(torch.norm(e1-e2, p=2, dim=-1)) #TODO : change to accomodate triplet loss
+            norm_l.append((torch.norm(e1, dim=-1) + torch.norm(e2, dim=-1))*loss_fn.alpha)
+    d = torch.concat(d_l)
+    y = torch.concat(y_l)
+    norm = torch.concat(norm_l)
+    metrics = {
+        'dist':{
+            'pos' : ((d*y).sum()/y.sum()).item(),
+            'neg' : ((d* ~y).sum()/y.sum()).item()
+            },
+        'l2_penalty':norm.mean().item(),
+        'loss' : np.mean( [t.detach().cpu().item() for t in loss_l]),
+        'acc' : np.mean([t.detach().cpu().item() for t in acc_l]),
+    }
+    return metrics
 
 def main(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -92,11 +122,20 @@ def main(args):
     dataframes = split(counts)
     train, test_seen, test_unseen = make_loaders(*dataframes, batch_size=args.batch_size,  )
     in_shape = next(iter(train))[0][0].shape[1]
-    model = Siamese(MLP(input_shape=in_shape)).to(device)
+    if args.restart :
+        print(f'Loading model from models/{args.run_name}.model.pkl')
+        model = torch.load(join('models',args.run_name+'.model.pkl'))
+        with open(join('models', args.run_name+'.meta.json'), 'r') as reader:
+            model_args = json.load(reader)
+    else:
+        model = Siamese(MLP(input_shape=in_shape)).to(device)
+        model_args = {
+            'restart':0,
+        }
     print(model)
     loss_fn = loss_dict[args.loss](margin=args.margin, alpha=args.alpha)
-    train_model(train, test_seen, test_unseen, model, loss_fn, device, 
-                margin=args.margin, run_name=args.run_name)
+    train_model(train, test_seen, test_unseen, model, model_args, loss_fn, device, 
+                margin=args.margin, run_name=args.run_name, )
 
 
 if __name__ == '__main__':
@@ -107,6 +146,7 @@ if __name__ == '__main__':
     parser.add_argument('-m','--margin',default=1, type=float, help='Contrastive loss margin parameter')
     parser.add_argument('-a','--alpha',default=1e-2, type=float, help='L2 embedding regularization')
     parser.add_argument('--batch-size',default=128, help='Batch size', type=int)
+    parser.add_argument('--restart', action='store_true')
     parser.add_argument('run_name',)
     args = parser.parse_args()
     main(args)
