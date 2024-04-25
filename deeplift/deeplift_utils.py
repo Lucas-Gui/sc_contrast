bdee9d8515a007cb33166adffcdfde14842100af
Lucas Guirardel <s2541999@ed.ac.uk>
2024-04-25 08:37:34 +0200

    Split deeplift files to minimize needed packages

--- deeplift/deeplift_utils.py
+++ deeplift/deeplift_utils.py
@@ -2,40 +2,13 @@
 import pandas as pd
 import numpy as np
 import time
-from typing import Tuple
+from typing import Literal
 
 import matplotlib.pyplot as plt
 import seaborn as sb
 
 from scipy.stats import uniform
 from sklearn.decomposition import PCA
-
-import tensorflow as tf
-import tensorflow.keras.layers as layers 
-
-## DEEPLIFT MODEL LOADING
-
-def make_model(X, shape=(40,40), activation='sigmoid', n_components_pca=40, n_classes=2):
-    pc_mat = PCA(n_components_pca).fit(X).components_.T
-    layer_pca = layers.Dense(n_components_pca, use_bias=False, activation=None)
-    layer_pca.trainable=False #freezing the layer
-    layer_pca(X[0:1]) #initializing the layer
-    layer_pca.set_weights((pc_mat,))
-    
-    model = tf.keras.Sequential(
-        [
-        layers.InputLayer((X.shape[1])), #unknown for deeplift...
-        layer_pca,
-        ]
-    )
-    for s in shape:
-        model.add(layers.Dense(s,activation=activation))
-    model.add(layers.Dense(n_classes, activation='softmax')) #the choice of softmax is debatable
-    opt = tf.keras.optimizers.Adam(learning_rate=1e-2)
-    loss = tf.keras.losses.CategoricalCrossentropy()
-    model.compile(opt, loss, metrics=[tf.keras.metrics.CategoricalAccuracy()])
-
-    return model
 
 ## PLOTS
 def confusion_matrix(Y_true : pd.DataFrame, Y_pred : pd.DataFrame, 
@@ -82,7 +55,7 @@ def remove_cst(df : pd.DataFrame, thresh = 5e-3) -> pd.DataFrame:
         count[sample.abs() > 1e-3] +=1
     return df.loc[:,count > thresh * df.shape[0]]
 
-def get_p53(dataset_path, labelled, normalize, thresh):
+def get_p53(dataset_path, labelled, normalize:Literal['standard','max','mean'], thresh):
     with Timer('Read csv'):
         df = pd.read_csv(dataset_path, index_col=0, dtype={'allele':str, 'Radiation':pd.Int64Dtype(), 'Drug':pd.Int64Dtype()})
         X = df.iloc[:,:-3]
